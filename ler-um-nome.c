c64634181146ec3fe5dddf084aa4b818de8fc107
Diogenes Oliveira Junior <contato@diogenesjunior.com.br>
2016-04-20 22:09:46 -0300

    Added files via upload

---
+++ ler-um-nome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char main(){
+
+char $nome[10];
+
+system("clear");
+printf("Digite seu nome: ");
+scanf("%s",$nome);
+system("clear");
+
+printf("Bem vindo %s \n\n\n",$nome);
+
+
+
+}
---
+++ scanf-numeros-inteiros.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int $saida;
+
+system("clear");
+printf("Digite um número: ");
+scanf("%d",&$saida);
+system("clear");
+
+printf("O número digitado foi: %d \n\n\n",$saida);
+
+return 0;
+
+}
